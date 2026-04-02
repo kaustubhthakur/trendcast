324bc13bbe84bf415821e91db3a750f2cf3f4679
Kaustubh <kaustubhthakur76@gmail.com>
2026-04-03 00:05:50 +0530

    authentication

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 const express = require('express')
 require('dotenv').config();
+const authrouter = require('./routes/auth')
+const cors = require('cors')
+
 const PORT = 8081;
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
-app.listen(PORT,()=>{console.log(`server is running on port ${PORT}...`)})
+app.use('/auth',authrouter);
+app.listen(PORT, () => { console.log(`server is running on port ${PORT}...`) })
